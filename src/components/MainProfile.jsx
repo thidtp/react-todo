503aa5fefdbff664ee18e3842e34da765304b153
thidtp <thidtp.02@gmail.com>
2025-04-02 17:56:27 +0700

    Add profile

--- src/components/MainProfile.jsx
+++ src/components/MainProfile.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import Avatar from '../assets/images/profile.png';
+import '../styles/Main.css';
 const MainProfile = () => {
     return (
-        <div>
-            <h2>sdsd</h2>
+        <div className='container'>
+            <div className='sidebar'>
+                <div className='profile'>
+                    <img src={Avatar} alt="Avatar" />
+                    <p className='name-profile'>Hey, ABCD</p>
+                </div>
+                <div className='menu-sidebar'>
+                    <button>All Tasks</button>
+                    <button>Today</button>
+                    <button>Important</button>
+                </div>
+                <div className='stats'>
+
+                </div>
+            </div>
+
         </div>
     )
 }
